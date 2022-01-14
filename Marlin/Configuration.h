f4a262439d41b831f9688947fa9b47f9bf64f78a
Jonas Kelley <jonas@jonaskelley.com>
2022-01-13 20:00:16 -0500

    Changed bed and E0 PID settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -601,9 +601,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  23.07
-    #define DEFAULT_Ki   2.49
-    #define DEFAULT_Kd 53.38
+    #define DEFAULT_Kp  25.59
+    #define DEFAULT_Ki   1.82
+    #define DEFAULT_Kd 90.15
   #endif
 #endif // PIDTEMP
 
@@ -642,9 +642,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-    #define DEFAULT_bedKp 56.30
-    #define DEFAULT_bedKi 11.08
-    #define DEFAULT_bedKd 190.63
+    #define DEFAULT_bedKp 57.98
+    #define DEFAULT_bedKi 10.89
+    #define DEFAULT_bedKd 205.83
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
